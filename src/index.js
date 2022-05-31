48bee0fb6a5da39e247846d6b1c2dd34b8ee5ee4
cloud-ya <claudia.huegi@gmail.com>
2022-05-31 21:23:57 +0200

    Fix apiUrl link from 3.0 to 2.5 (free)

--- src/index.js
+++ src/index.js
@@ -21,25 +21,26 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class = "row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
-  days.forEach(function (day) {
+  forecast.forEach(function (forecastDay) {
     forecastHTML =
       forecastHTML +
       `
     <div class="col-2" id="forecast">
-      <div class="weather-forecast-date">${day}</div>
+      <div class="weather-forecast-date">${forecastDay.dt}</div>
       <img
-        src="http://openweathermap.org/img/wn/02d@2x.png"
+        src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
         alt=""
         width="50"
       />
       <div class="weather-forecast-temperature">
-        <span class="weather-forecast-temperature-max">18°</span>
-        <span class="weather-forecast-temperature-min">12°</span>
+        <span class="weather-forecast-temperature-max">${forecastDay.temp.max}°</span>
+        <span class="weather-forecast-temperature-min">${forecastDay.temp.min}</span>
       </div>
     </div>
   `;
@@ -50,9 +51,9 @@ function displayForecast() {
 }
 
 function getForecast(coordinates) {
+  console.log(coordinates);
   let apiKey = "bc8c878e9acb5588f7a09bb59fd2ee98";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
